3ab44a335271ade2475b57cea34c8d15fbed2a07
theflyingmantis <rai.1@iitj.ac.in>
2015-11-01 22:23:16 +0530

    value function not working

---
+++ static/value.js
@@ -0,0 +1,26 @@
+function value()
+{
+	console.log("In value.YO!!");
+	var dlr=document.getElementById("dealers").value;
+	console.log(dlr);
+	$.ajax({
+			type:"GET",//Check and change it to GET
+			url:"/ajax_admin",
+			data: {dealer: dlr},
+
+			success: function(answer)
+			{
+				console.log(answer);
+				//console.log(answer.comments,answer.id1);
+				alert("Change Ackonowledge");
+			},
+			error: function(err)
+			{
+				console.log(err);
+				alert(err);
+				alert("Error");
+			}
+		});
+
+
+}
